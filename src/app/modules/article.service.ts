747eee459b8821aa09fb38f578ce6e61f51d9de7
jayasingh24 <96876047+jayasingh24@users.noreply.github.com>
2024-05-04 22:03:02 +0530

    updated

--- src/app/modules/article.service.ts
+++ src/app/modules/article.service.ts
@@ -14,11 +14,11 @@ export class ArticleService {
 
   articles = [
 
-    { id: 1, title: 'Article 1', content: 'Content of Article 1' },
+    { id: 1, title: 'Article 1', content: 'Content of Article 1',subContent:"check1" },
 
-    { id: 2, title: 'Article 2', content: 'Content of Article 2' },
-    { id: 3, title: 'Article 3', content: 'Content of Article 3' },
-    { id: 4, title: 'Article 4', content: 'Content of Article 4' },
+    { id: 2, title: 'Article 2', content: 'Content of Article 2' ,subContent:"check2"},
+    { id: 3, title: 'Article 3', content: 'Content of Article 3',subContent:"check3" },
+    { id: 4, title: 'Article 4', content: 'Content of Article 4',subContent:"check4" },
 
   
 
