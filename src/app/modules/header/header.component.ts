71d50133d6f62e7b8ae236a3097d08b97c6193c6
jayasingh24 <96876047+jayasingh24@users.noreply.github.com>
2024-05-06 01:38:42 +0530

    up

--- src/app/modules/header/header.component.ts
+++ src/app/modules/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,ElementRef, HostListener  } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  constructor(private el: ElementRef) {}
 
+  @HostListener('document:click', ['$event'])
+  clickout(event: Event) {
+    console.log('Click detected');
+    if (!this.el.nativeElement.contains(event.target)) {
+      console.log('Clicked outside of navbar');
+      const navbarCollapse = this.el.nativeElement.querySelector('.navbar-collapse');
+      const navbarToggler = this.el.nativeElement.querySelector('#navbar-toggler-btn');
+      
+      if (navbarCollapse.classList.contains('show') && event.target !== navbarToggler) {
+        console.log('Closing navbar');
+        navbarCollapse.classList.remove('show');
+      }
+    }
+  }
+  
 }
